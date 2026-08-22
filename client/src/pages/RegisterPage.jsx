bf23b8f6178c549fd5e28f2475b8337725730f4b
muraddavid97-eng <muraddavid97@gmail.com>
2026-08-22 20:16:34 +0300

    reg3

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -21,7 +21,7 @@ function RegisterPage() {
 
         navigate("/register/children", { state: { registrationData: formData } });
     };
-//register
+
     return (
         <AuthLayout
             activeView="register"
